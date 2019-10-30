dd5c480f50f81cfb874b4aa7aec7c30e7c1e9e74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-30 15:07:31 +0000

    Deploy baitun/ck2zm to github.com/baitun/ck2zm.git:gh-pages

---
+++ precache-manifest.2b83aa15c15d71dc4f44f3bbe45130c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49476c94c94e3750b78705c29c8dab14",
+    "url": "/ck2zm/index.html"
+  },
+  {
+    "revision": "e75432204d1d69b4e791",
+    "url": "/ck2zm/static/css/main.0fbe281d.chunk.css"
+  },
+  {
+    "revision": "36f5e85fa21d8f1ff63b",
+    "url": "/ck2zm/static/js/2.e8dba672.chunk.js"
+  },
+  {
+    "revision": "e75432204d1d69b4e791",
+    "url": "/ck2zm/static/js/main.e8ba84f1.chunk.js"
+  },
+  {
+    "revision": "976fbf8001d0c9f72939",
+    "url": "/ck2zm/static/js/runtime~main.2033e85f.js"
+  }
+]);
